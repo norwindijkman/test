355fb949bff6c0757daf4d0f7445986ee3dbe74b
Norwin Dijkman <norwin.dijkman@plathena.com>
2021-07-01 15:08:50 +0200

    add playlist and video component

---
+++ components/Playlist/Playlist.js
@@ -0,0 +1,18 @@
+import Video from '../Video/Video'
+import styles from './Playlist.module.css'
+
+export default function Playlist({ playlist }) {
+  // Since the playlist is prerendered, rendering the website should fail if the playlist is not found.
+  if (!Array.isArray(playlist.playlist)) {
+    throw 'Video playlist not found. Check if the API is still valid.'
+  }
+  return (
+    <div>
+      {playlist.playlist.map((video) => (
+        <div key={video.mediaid}>
+          <Video video={video} />
+        </div>
+      ))}
+    </div>
+  )
+}
---
+++ components/Video/Video.js
@@ -0,0 +1,9 @@
+import styles from './Video.module.css'
+
+export default function Video({ video }) {
+  return (
+    <div>
+      {video.title}
+    </div>
+  )
+}
