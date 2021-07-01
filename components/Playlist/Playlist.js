d65cae1d7bf329594d7a555a01da6d5b8aecfdc4
Norwin Dijkman <norwin.dijkman@plathena.com>
2021-07-01 16:34:24 +0200

    adjust Video and Playlist component

--- components/Playlist/Playlist.js
+++ components/Playlist/Playlist.js
@@ -4,7 +4,7 @@ import styles from './Playlist.module.css'
 export default function Playlist({ playlist }) {
   // Since the playlist is prerendered, rendering the website should fail if the playlist is not found.
   if (!Array.isArray(playlist.playlist)) {
-    throw 'Video playlist not found. Check if the API is still valid.'
+    throw new Error('Video playlist not found. Check if the API is still valid.')
   }
   return (
     <div>
--- components/Video/Video.js
+++ components/Video/Video.js
@@ -10,23 +10,27 @@ const format = {
 export default function Video({ video }) {
   // Since the playlist is prerendered, rendering the website should fail if the sources of the video are not found.
   if (!Array.isArray(video.sources) || !video.sources.length) {
-    throw 'Video sources not found for ' + video.title + '. Check if the API is still valid.'
+    throw new Error('Video sources not found for ' + video.title + '. Check if the API is still valid.')
   }
   return (
     <div>
       <div>
         {video.title}
       </div>
-      <video controls>
+      <video poster={video.image} width="100%" controls>
         {video.sources.map(source => (
-          <source key={source.file} src={source.file} type={source.type} />
+          <source
+            key={source.file}
+            src={source.file}
+            type={source.type} />
         ))}
+        <p>Uw browser ondersteunt geen HTML5-video. Hier is in plaats daarvan een <a href={video.link}>link naar de video</a>.</p>
       </video>
       <div>
         {video.duration} min
       </div>
       <div>
-        {new Date(video.pubdate).toLocaleDateString('nl-NL', format)}
+        {new Date(video.pubdate*1000).toLocaleDateString('nl-NL', format)}
       </div>
     </div>
   )
